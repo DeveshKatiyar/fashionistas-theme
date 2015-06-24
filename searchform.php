db75276e235712e09b6b20faec7a86b9e8b4cecb
cpate <cpate9@gmail.com>
2015-06-24 11:18:29 +0200

    Attempt1
    Search the style.css for “Search Button” to jump to the part changed.
    Currently displays a box instead of the font awesome icon.

--- searchform.php
+++ searchform.php
@@ -4,11 +4,29 @@
  *
  * @package aThemes
  */
+ 
 ?>
+<head>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'athemes' ); ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Enter Search Text', 'placeholder', 'athemes' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'athemes' ); ?>">
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Boo', 'submit button', 'athemes' ); ?>">
+
+	<input type="submit" class="search-submit" value="&#xf002;"> 
+	
+	
 </form>
+
+<!-- Font Awesome search icon: &#xf002; 
+Other things I've tried: 
+<button type="submit" class="btn btn-success">
+                <i class="icon-circle-arrow-right icon-large"></i> GO
+            </button> 
+This actually give a button there...soooooo
+            -->
+
+
