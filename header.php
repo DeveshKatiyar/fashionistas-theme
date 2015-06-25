bcca33a32cf00f9d8fd454cdbbd2cff0d3f63905
cpate <cpate9@gmail.com>
2015-06-25 13:01:42 +0200

    More changes with FA
    SearchIcon and SocialMedia Icons

--- header.php
+++ header.php
@@ -10,6 +10,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
